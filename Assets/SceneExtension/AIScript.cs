d3c6b6588b4981808aad0fa997106ee051a9c98b
MonsieurSadie <kevin.lesur@gmail.com>
2021-01-22 18:17:34 +0100

    Scene handles example

---
+++ Assets/SceneExtension/AIScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class AIScript : MonoBehaviour
+{
+  public enum State
+  {
+  	IDLE,
+  	MOVING,
+  	EATING,
+  	SURFING,
+  	USA
+  }
+
+  public State state = State.IDLE;
+  float speed = 1;
+  public Vector3 target;
+
+  public float visionAngle = 60;
+  public float visionDistance = 5;
+
+  void Start()
+  {
+  	// on change de state toutes les 5 secondes
+    InvokeRepeating("ChangeState", 5, 10);
+  }
+
+  void Update()
+  {
+  	if(state == State.MOVING)
+  	{
+  		transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+  	}
+  }
+
+  void ChangeState()
+  {
+    int currentState = (int)state;
+    currentState++;
+    if(currentState > (int)State.USA) currentState = 0;
+    state = (State) currentState;
+
+    if(state == State.MOVING)
+    {
+    	target = Random.onUnitSphere * 5;
+    }
+  }
+}
---
+++ Assets/SceneExtension/Editor/AICustomEditor.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(AIScript))]
+public class AICustomEditor : Editor
+{
+  void OnSceneGUI()
+  {
+  	AIScript ai = (AIScript)target;
+
+    if (ai == null)
+    {
+        return;
+    }
+
+  	Handles.Label(ai.transform.position, ai.state.ToString());
+
+  	if(ai.state == AIScript.State.MOVING)
+  	{
+  		// draw a line towards target
+  		float dotLineSpacesInPixel = 4;
+  		Handles.DrawDottedLine(	ai.transform.position, 
+  														ai.target, 
+  														dotLineSpacesInPixel);
+  	}
+
+
+  	// dessiner le cone de vision de l'IA
+  	Handles.color = new Color(0.1f, 0.3f, 0.7f, 0.6f);
+  	// la version suivante fait un arc de vision qui part du forward et va vers la droite
+  	//Handles.DrawSolidArc(ai.transform.position, ai.transform.up, ai.transform.forward, ai.visionAngle, ai.visionDistance);
+
+  	// si on veut que l'arc soit symétrique par rapport au forward, il faut faire démarrer à gauche du forward, de la moitié de l'arc
+  	Quaternion halfAngleRot = Quaternion.AngleAxis(-ai.visionAngle*0.5f, ai.transform.up);
+  	Vector3 arcStartDir = halfAngleRot * ai.transform.forward;
+		Handles.DrawSolidArc(ai.transform.position, ai.transform.up, arcStartDir, ai.visionAngle, ai.visionDistance);  	
+  }
+}
